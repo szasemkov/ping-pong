f5eef03f84b833c68a27a03b6774dfab2ea42fd3
Sergey Zasemkov <szasemkov@gmail.com>
2024-07-12 09:32:04 +0200

    init commit

---
+++ pong-service/src/main/java/com/szasemkov/pong/PongServiceApplication.java
@@ -0,0 +1,13 @@
+package com.szasemkov.pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongServiceApplication.class, args);
+	}
+
+}
---
+++ pong-service/src/test/java/com/szasemkov/pong/PongServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szasemkov.pong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PongServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
